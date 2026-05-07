c8a51349b38997feb4537322ee162ecba566a7b0
Marcel Roozekrans <marcel@roozekrans.nl>
2026-05-07 14:43:14 +0200

    chore(scaffold): generator project + analyzer-bundling MSBuild target

---
+++ src/ZeroAlloc.Mapping.Generator/MappingGenerator.cs
@@ -0,0 +1,10 @@
+namespace ZeroAlloc.Mapping.Generator;
+
+[Microsoft.CodeAnalysis.Generator(Microsoft.CodeAnalysis.LanguageNames.CSharp)]
+public sealed class MappingGenerator : Microsoft.CodeAnalysis.IIncrementalGenerator
+{
+    public void Initialize(Microsoft.CodeAnalysis.IncrementalGeneratorInitializationContext context)
+    {
+        // Emission added in Tasks 11-16.
+    }
+}
