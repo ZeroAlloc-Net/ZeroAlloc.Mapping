d3bb8768f23e60cc4b73e1dd8d282084cb1411f3
Marcel Roozekrans <marcel@roozekrans.nl>
2026-05-09 20:50:01 +0200

    feat(bench): add hand-written baseline mappers

---
+++ benchmarks/ZeroAlloc.Mapping.Benchmarks/Mappers/HandWritten.cs
@@ -0,0 +1,41 @@
+using ZeroAlloc.Mapping.Benchmarks.Models;
+
+namespace ZeroAlloc.Mapping.Benchmarks.Mappers;
+
+public static class HandWritten
+{
+    public static FlatDst MapFlat(FlatSrc s) => new(
+        s.Id, s.Name, s.Email, s.Age, s.Active, s.Score, s.Version, s.Country);
+
+    public static ConvDst MapConv(ConvSrc s) => new(
+        new OrderId(int.Parse(s.Id, System.Globalization.CultureInfo.InvariantCulture)),
+        Enum.Parse<Status>(s.Status),
+        s.Count,
+        DateTime.Parse(s.Created, System.Globalization.CultureInfo.InvariantCulture));
+
+    public static OrderFlat MapFlatten(OrderSrc s) => new(
+        s.OrderId, s.Customer.Id, s.Customer.Name,
+        s.Customer.Address.Street, s.Customer.Address.City, s.Customer.Address.Zip,
+        s.Total);
+
+    public static List<FlatDst> MapList(List<FlatSrc> src)
+    {
+        var dst = new List<FlatDst>(src.Count);
+        for (var i = 0; i < src.Count; i++) dst.Add(MapFlat(src[i]));
+        return dst;
+    }
+
+    public static AnimalDto MapAnimal(Animal a) => a switch
+    {
+        Dog d => new DogDto(d.Name, d.Breed),
+        Cat c => new CatDto(c.Name, c.Indoor),
+        Bird b => new BirdDto(b.Name, b.Wingspan),
+        _ => throw new NotSupportedException()
+    };
+
+    public static void UpdateInPlace(FlatSrc s, FlatDstMutable d)
+    {
+        d.Id = s.Id; d.Name = s.Name; d.Email = s.Email; d.Age = s.Age;
+        d.Active = s.Active; d.Score = s.Score; d.Version = s.Version; d.Country = s.Country;
+    }
+}
